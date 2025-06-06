d2dd1d9e8a8ddc02be73fccbcfb174d712564681
Fedora-MISTAKELY <thereisamailforme@gmail.com>
2025-06-06 23:04:11 +0545

    solved the error of login and signup

--- backend/nodemailer/nodemailer.config.js
+++ backend/nodemailer/nodemailer.config.js
@@ -3,26 +3,17 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-// export const transporter = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: process.env.USER,
-//     pass: process.env.PASS,
-//   },
-// });
-
-
 export const transporter = nodemailer.createTransport({
   // service: "gmail",
   host:'smtp.gmail.com',
   port: 465,
-  secure: true, // true for 465, false for other ports
+  secure: true,
   auth: {
     user: process.env.MAILTRAP_USER,
     pass: process.env.MAILTRAP_PASS,
   },
-  logger: true,  // add this
+  logger: true, 
   debug: true,
 });
 
-export const sender = `"ASN" <no-reply@yourapp.com>`; // Displayed sender name/email
+export const sender = `"ASN" <no-reply@yourapp.com>`;
